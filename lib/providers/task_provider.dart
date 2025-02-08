a312a4872058fab98181825cf44e3b345d94667e
frankesomcarem <wakasiakafrankline@gmail.com>
2025-02-08 21:39:39 +0300

    added task prider file

---
+++ lib/providers/task_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/task.dart';
+import '../repositories/task_repository.dart';
+
+final taskRepositoryProvider = Provider((ref) => TaskRepository());
+final taskProvider = StateNotifierProvider((ref) => TaskNotifier(ref.read(taskRepositoryProvider)));
+class TaskNotifier extends StateNotifier<List<Task>> {
+  final TaskRepository _taskRepository;
+
+  TaskNotifier(this._taskRepository) : super([]);
+
+  void addTask(Task task) {
+    _taskRepository.addTask(task);
+    state = [...state, task];
+  }
+
+  void updateTask(Task task) {
+    _taskRepository.updateTask(task);
+    state = state.map((t) => t.id == task.id ? task : t).toList();
+  }
+
+  void deleteTask(String id) {
+    _taskRepository.deleteTask(id);
+    state = state.where((t) => t.id != id).toList();
+  }
+
+  void getTasks() {
+    state = _taskRepository.getTasks();
+  }
+}
