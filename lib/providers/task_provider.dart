f823b2ed25b21e3934534b4f59808a9cd5408278
frankesomcarem <wakasiakafrankline@gmail.com>
2025-02-09 20:13:00 +0300

    tracking online  and syncing states

--- lib/providers/task_provider.dart
+++ lib/providers/task_provider.dart
@@ -4,19 +4,31 @@ import '../models/task.dart';
 import '../repositories/task_repository.dart';
 
 final taskRepositoryProvider = Provider((ref) => TaskRepository());
-final taskListProvider = StateNotifierProvider<TaskListNotifier, List<Task>>((ref){
+final taskListProvider =
+    StateNotifierProvider<TaskListNotifier, List<Task>>((ref) {
   return TaskListNotifier(ref.watch(taskRepositoryProvider));
 });
+
 class TaskListNotifier extends StateNotifier<List<Task>> {
   final TaskRepository _taskRepository;
+  bool _isSyncing = false; //track the syncing process state
+  bool _isOnline = false; //track offline or online status
 
-  TaskListNotifier(this._taskRepository) : super([]){
-    getTasks();//fetch tasks from the repository on initialisation
+  TaskListNotifier(this._taskRepository) : super([]) {
+    getTasks(); //fetch tasks from the repository on initialisation
+  }
+
+  //toggle online and offline mode
+  void toggleOnlineMode() {
+    _isOnline = !_isOnline;
+    if (_isOnline) {
+      syncTasks(); //sync tasks from firestore when online
+    }
   }
 
   void addTask(Task task) {
     _taskRepository.createTask(task);
-    state = [...state, task];//update the state with new task
+    state = [...state, task]; //update the state with new task
   }
 
   void updateTask(Task task) {
@@ -26,22 +38,31 @@ class TaskListNotifier extends StateNotifier<List<Task>> {
 
   void deleteTask(String id) {
     _taskRepository.deleteTask(id);
-    state = state.where((t) => t.id != id).toList();//removes the task from the state
+    state = state
+        .where((t) => t.id != id)
+        .toList(); //removes the task from the state
   }
+
   void completeTask(String id) {
     _taskRepository.completeTask(id);
     final task = state.firstWhere((t) => t.id == id);
-    task.isDone = true;//mark the task as completed
+    task.isDone = true; //mark the task as completed
     updateTask(task); //update task in the state
-
-     }
+  }
 
   void getTasks() {
     state = _taskRepository.getTasks();
   }
+
   //Sync tasks from firestore
-Future<void>syncTasks()async{
-    await _taskRepository.syncTasksFromFirestore();
-    getTasks();//refresh the state with the updated tasks
-}
+  Future<void> syncTasks() async {
+    if (_isOnline) {
+      _isSyncing = true; //set syncing state to true
+      state = [...state]; //refresh the state
+      await _taskRepository.syncTasksFromFirestore(); // sync with firestore
+
+      getTasks(); //refresh the state with the updated tasks
+      _isSyncing = false; //set syncing state to false
+    }
+  }
 }
